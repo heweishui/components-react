083d2ec1f365d9cbcca16a48d4e32e3996729d5b
heweishui <zhoushihao0126@163.com>
2024-02-29 09:44:47 +0800

    fix: 修改组件库名称和入口文件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 "use client";
 import styles from "./page.module.css";
-import { Input } from "../components-react/dist";
+import { Input } from "harmony-components-react";
 import { SetStateAction, useState } from "react";
 
 export default function Home() {
@@ -12,11 +12,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div>
-        <Input
-          title="标题"
-          type={"text"}
-          onPressEnter={handleEnter}
-        ></Input>
+        <Input title="标题" type={"text"} onPressEnter={handleEnter}></Input>
         <p>组件输入数据：{val}</p>
       </div>
     </main>
